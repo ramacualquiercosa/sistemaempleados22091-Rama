ab139424dfed4dc976a290248b7573b47b09188e
Rama <ramiroacosta2213@gmail.com>
2022-07-02 14:24:55 -0300

    02/07/2022

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template , request , redirect
 from flaskext.mysql import MySQL
 from datetime import datetime
+import os
 
 app=Flask(__name__)
 
@@ -12,6 +13,8 @@ app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'sistema22091'
 mysql.init_app(app)
 
+CARPETA = os.path.join('uploads')
+app.config['CARPETA']=CARPETA
 
 @app.route("/")
 def index():
@@ -28,9 +31,56 @@ def index():
 def destroy(id):
     conn = mysql.connect()
     cursor=conn.cursor()
+    cursor.execute("SELECT foto FROM empleados WHERE id =%s",id)
+    fila=cursor.fetchall()
+    print(fila[0][0])
+    os.remove(os.path.join(app.config['CARPETA'],fila[0][0] ))
     cursor.execute("DELETE FROM empleados WHERE id=%s",(id))
     conn.commit() 
     return redirect('/')
+
+@app.route("/edit/<int:id>")
+def edit(id):
+    conn = mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute("SELECT * FROM empleados WHERE id =%s", (id))
+    empleados =  cursor.fetchall()
+    print(empleados)
+    conn.commit()  
+    return render_template('empleados/edit.html' , empleados=empleados)
+
+@app.route("/update" , methods=['POST'])
+def update():
+    _nombre = request.form['txtNombre']
+    _correo = request.form['txtCorreo']
+    _foto   = request.files['txtFoto']
+    _id     =   request.form['txtId']
+ 
+    sql = "UPDATE empleados SET nombre=%s , correo=%s WHERE id=%s;" 
+    datos = (_nombre ,_correo,_id)
+    conn = mysql.connect()
+    cursor=conn.cursor()
+
+    now   =  datetime.now()
+    ahora =  now.strftime("%Y%H%M%S")
+    nuevoNombreFoto = ''
+    
+    if _foto.filename != '':
+        nuevoNombreFoto = ahora + _foto.filename
+        _foto.save('uploads/' + nuevoNombreFoto)
+
+        cursor.execute("SELECT foto FROM empleados WHERE id =%s",_id)
+        fila=cursor.fetchall()
+
+        print(fila[0][0])
+        os.remove(os.path.join(app.config['CARPETA'],fila[0][0] ))
+        cursor.execute("UPDATE empleados SET foto=%s WHERE id=%s ",(nuevoNombreFoto , _id))
+        conn.commit()
+
+    cursor.execute(sql, datos)
+    conn.commit()
+    return redirect('/')
+
 
 @app.route("/create")
 def create():
